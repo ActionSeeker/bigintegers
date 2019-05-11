9506a3b842a1c05f213746055681d6a6d96d5f8f
Luv Karakoti <luvkarakoti@Luvs-MacBook-Air.local>
2019-05-11 19:20:08 +0100

    Added test cases for Zero

--- test/class/biginteger.test.js
+++ test/class/biginteger.test.js
@@ -3,25 +3,42 @@ const { BigInteger } = require('../../dist/class/biginteger');
 
 let bigInteger;
 
+const PLUS = '+';
+const MINUS = '-';
+
 describe('BigInteger class tests', () => {
     it('Should parse a normal integer value correct', () => {
-        const NORMAL_INTEGER_CASE = '12481738958901379857359872';
-        bigInteger = new BigInteger(NORMAL_INTEGER_CASE);
-        expect(bigInteger.integer).to.equal(NORMAL_INTEGER_CASE);
-        expect(bigInteger.sign).to.equal('+');
+        const REGULAR_BIG_INT = '12481738958901379857359872';
+        bigInteger = new BigInteger(REGULAR_BIG_INT);
+        expect(bigInteger.integer).to.equal(REGULAR_BIG_INT);
+        expect(bigInteger.sign).to.equal(PLUS);
     });
 
     it('Should parse a negative integer value correct', () => {
-        const NEGATIVE_INTEGER_CASE = '-893289578927458939578273857984768';
-        bigInteger = new BigInteger(NEGATIVE_INTEGER_CASE);
-        expect(bigInteger.integer).to.equal(NEGATIVE_INTEGER_CASE);
-        expect(bigInteger.sign).to.equal('-');
+        const NEGATIVE_BIG_INT = '-893289578927458939578273857984768';
+        bigInteger = new BigInteger(NEGATIVE_BIG_INT);
+        expect(bigInteger.integer).to.equal(NEGATIVE_BIG_INT);
+        expect(bigInteger.sign).to.equal(MINUS);
     })
 
     it('Should remove zeroes before the number', () => {
         const ZEROES_CASE = '-0012878';
         bigInteger = new BigInteger(ZEROES_CASE);
         expect(bigInteger.integer).to.equal('-12878');
-        expect(bigInteger.sign).to.equal('-');
+        expect(bigInteger.sign).to.equal(MINUS);
+    })
+
+    it('Should parse 0 as ZERO', () => {
+        const ZERO = '0';
+        bigInteger = new BigInteger(ZERO);
+        expect(bigInteger.integer).to.equal(BigInteger.ZERO);
+        expect(bigInteger.sign).to.equal(PLUS);
+    })
+
+    it('Should parse a trail of zeros as ZERO', () => {
+        const ZERO = '00000000';
+        bigInteger = new BigInteger(ZERO);
+        expect(bigInteger.integer).to.equal(BigInteger.ZERO);
+        expect(bigInteger.sign).to.equal(PLUS);
     })
 });
