090f4ec71a65646072ed99a71ed25ab35c2598a4
Luv Karakoti <luvkarakoti@Luvs-MacBook-Air.local>
2019-05-19 10:17:03 +0100

    Updated description of test cases

--- test/class/biginteger.add.test.js
+++ test/class/biginteger.add.test.js
@@ -2,14 +2,14 @@ const { expect } = require('chai');
 const { BigInteger } = require('../../dist/class/biginteger');
 
 describe('BigInteger addition tests', () => {
-    it('Should add a>0 and b>0', () => {
+    it('Should add a>0 and b>0 as a + b = a + b where a, b are absolute values', () => {
         const ersteNummer = new BigInteger('293859348968935374917348781374');
         const zweiteNummer = new BigInteger('130589427635823812789479829');
         const result = ersteNummer.add(zweiteNummer);
         expect(result.toString()).to.equal('293989938396571198730138261203');
     });
 
-    it('Should add a<0 and b<0', () => {
+    it('Should add a<0 and b<0 as -a+(-b) = -(a + b) where a, b are absolute values', () => {
         const ersteNummer = new BigInteger('-8468938597604850978496087949056');
         const zweiteNummer = new BigInteger('-49609350790459079804580789350960');
         const result = ersteNummer.add(zweiteNummer);
--- test/class/biginteger.minus.test.js
+++ test/class/biginteger.minus.test.js
@@ -2,14 +2,14 @@ const { expect } = require('chai');
 const { BigInteger } = require('../../dist/class/biginteger');
 
 describe('BigInteger addition tests', () => {
-    it('Should add a>0 and b<0', () => {
+    it('Should add a>0 and b<0 -a-b) = -(a + b) where a, b are absolute values', () => {
         const ersteNummer = new BigInteger('293859348968935374917348781374');
         const zweiteNummer = new BigInteger('-130589427635823812789479829');
         const result = ersteNummer.minus(zweiteNummer);
         expect(result.toString()).to.equal('293989938396571198730138261203');
     });
 
-    it('Should add a<0 and b>0', () => {
+    it('Should add a<0 and b>0 as a-(-b) = a + b where a, b are absolute values', () => {
         const ersteNummer = new BigInteger('-04957083059636339859230580989');
         const zweiteNummer = new BigInteger('03950284698930859273083598293');
         const result = ersteNummer.minus(zweiteNummer);
